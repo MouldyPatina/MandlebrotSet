b6e993bfe727fd81d162e8b2a055cfbe0080ec53
Mouldy Patina <MoudlyPatina@proton.me>
2024-03-04 22:54:29 +0000

    program creates a png file with Mandlebrot set

--- main.go
+++ main.go
@@ -2,35 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"image"
+	"image/color"
+	"image/png"
 	"math"
 	"strconv"
 )
 
 func main () {
-	picture := Picture(195, 70);
-	fmt.Println(picture);
+	Picture(2560, 1440, 500);
+	fmt.Println("see the image");
 }
 
-func Picture(widthSteps int, heightSteps int) string {
-	str := "";
+func Picture(widthSteps int, heightSteps int, accuracy int) {
+	upLeft := image.Point{0, 0};
+	lowRight := image.Point{widthSteps, heightSteps};
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight});
 	var height float64 =  1.0;
 	var width float64 =  2.1;
 	for y := 0; y <= heightSteps; y++ {
 		for x := 0; x <= widthSteps; x++ {
-			z := complexNum{real: -width + (width * float64(x) * 2) / float64(widthSteps), imagine: height - (height * 2 * float64(y)) / float64(heightSteps)}; 
-			z.BlowUp(2.5, 1);
-			if blowUp := z.BlowUp(2.5, 100000); blowUp == -1 {
-				str += "*";
-			} else if blowUp > 10{
-				str += " ";
+			z := complexNum{real: -width + (width * float64(x) * 3) / (2 * float64(widthSteps)), imagine: height - (height * 2 * float64(y)) / float64(heightSteps)}; 
+			if blowUp := z.BlowUp(float64(accuracy), 5 * accuracy); blowUp == -1 {
+				img.Set(x, y, color.Black);	
 			} else {
-				str += "-";
+				scale := (uint8) (100 - 100 / blowUp );
+				pixelColor := color.RGBA{ 2*scale, scale, scale, 0xff};
+				img.Set(x, y, pixelColor);
 			}
 		}
-		str += "\n";
 	}
-
-	return str;
+	f, _ := os.Create("MandleBrot_Set.png");
+	png.Encode(f, img);
 }
 
 func (start *complexNum) BlowUp(bound float64, numTries int) int {
